afe08fc4a2dd841baaf6ab24a03bf549c94cef71
root <fissaa.amin@gmail.com>
2025-01-15 06:53:07 -0800

    fixing minor issues

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
 # Function to display the menu
 def display_menu():
-    print("\nShopping List Manager")
+    print(f"Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
